2b9d8c411d1289cda1adb03336bf148bf247c5b8
Faii-GitHub <w.chinjaroenkit@github.com>
2020-04-29 19:07:39 +0700

    Create Exercise4_Warisara_C.py
    Collect score of 4 subjects in variables and show it all on the screen.

---
+++ Exercise4_Warisara_C.py
@@ -0,0 +1,10 @@
+F_Eng_Score = 84.73
+Gen_Bus_Score = 82.5
+Intro_Com_Sys_Score = 87.0
+Com_Programming_Score = 79.2
+print("----------------Your Score-----------------")
+print("Foundation English               :", F_Eng_Score)
+print("General Business                 :",Gen_Bus_Score)
+print("Introduction to Computer Systems :", Intro_Com_Sys_Score)
+print("Computer Programming             :", Com_Programming_Score)
+print("-------------------------------------------")
