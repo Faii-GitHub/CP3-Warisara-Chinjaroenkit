9738b833132c22667093b2fb092779c007a17579
Faii-GitHub <w.chinjaroenkit@github.com>
2020-04-30 00:13:28 +0700

    Create Exercise5_1_Warisara_C.py
    ใช้ตัวแปรมาทำการ บวก ลบ คูณ และ หาร ตัวเลข

---
+++ Exercise5_1_Warisara_C.py
@@ -0,0 +1,6 @@
+Number1 = 15
+Number2 = 3
+print(Number1,"+",Number2,"=",int(Number1+Number2))
+print(Number1,"-",Number2,"=",int(Number1-Number2))
+print(Number1,"*",Number2,"=",int(Number1*Number2))
+print(Number1,"/",Number2,"=",int(Number1/Number2))
